9ee8554e9edebb463b46f7398a8498d040f183ed
kaljan <kaljan.nothern@gmail.com>
2019-05-16 23:51:30 +0300

    [comments] added comments

--- nbrb_cur.js
+++ nbrb_cur.js
@@ -1,5 +1,9 @@
 /**
- * Пересчёт курсов валют
+ * @brief Пересчёт курсов валют
+ * 
+ * В этом файле собраны функции для перевода в разные валюты.
+ * 
+ * В коде использовано API от НБРБ http://www.nbrb.by/APIHelp/ExRates
  */
 
 var exchangeData = {
@@ -8,8 +12,13 @@ var exchangeData = {
 };
 
 /**
- * Получить JSON валюты по имени
+ * Получить объект валюты по аббривеатуре
  * 
+ * Отправляет запрос на сервер НБРБ:
+ * 
+ * http://www.nbrb.by/API/ExRates/Rates/USD?ParamMode=2
+ * 
+ * Получает JSON (пример ниже). Парсит его и превращает в объект.
  * {
  *   "Cur_ID":145, // ID валюты
  *   "Date":"2019-05-14T00:00:00", // дата
@@ -27,8 +36,11 @@ var exchangeData = {
  *   "Cur_Name":"Российских рублей",
  *   "Cur_OfficialRate":3.2080
  * }
- * http://www.nbrb.by/API/ExRates/Rates/UAH/?ParamMode=2
- * @param currency_name абревиатура валюты. пример 'usd'
+ * 
+ * 
+ * 
+ * @param abbr абревиатура валюты. пример 'usd'
+ * @returns в случае успеха возвращает объект валюты, иначе null
  */
 
 function NBRBFerchCurByAbbr(abbr) {
@@ -44,8 +56,9 @@ function NBRBFerchCurByAbbr(abbr) {
 }
 
 /**
+ * Вывести в лог объект
  * 
- * @param {*} table 
+ * @param {*} table любой объект
  */
 function printCurTable(table) {
 	var str = JSON.stringify(table, "", 2);
@@ -53,8 +66,10 @@ function printCurTable(table) {
 }
 
 /**
+ * Получает список объектов валюты из списка аббревиатур.
  * 
- * @param {*} curIDs 
+ * @param {*} curIDs список абривеатур валют, которые 
+ * нужно получить с сервера. пример 'usd'
  */
 function NBRBFetchCurrencies(curIDs) {
     curIDs.forEach(function(item) {
@@ -69,27 +84,31 @@ function NBRBFetchCurrencies(curIDs) {
 }
 
 /**
- * 
- * @param {*} cur 
- * @param {*} value 
+ * Конвертирует валлюту cur в белорусские рубли
+ * @param {*} cur 	валюта из которой нужно конвертировать
+ * @param {*} value значение в ин. валюте
+ * @returns значение в белорусских рублях
  */
 function NBRBConvertToBYN(cur, value) {
 	return value * (cur.Cur_OfficialRate / cur.Cur_Scale);
 }
 
 /**
- * 
- * @param {*} cur 
- * @param {*} value 
+ * Конвертирует белорусские рубли в валюту cur 
+ * @param {*} cur 	валюта, в которую нужно конвертировать. 
+ * @param {*} value значение в белоруских рублях
+ * @returns значение в валюте
  */
 function NBRBConvertFromBYN(cur, value) {
 	return value / (cur.Cur_OfficialRate / cur.Cur_Scale);
 }
 
 /**
+ * Конвертирует валюту в белорусские рубли по абривеатуре
  * 
- * @param {*} abbr 
- * @param {*} value 
+ * @param {*} abbr абревиатура валюты
+ * @param {*} value значение
+ * @returns значение в белорусских рублях
  */
 function NBRBExchangeToBYN(abbr, value) {
 	if (abbr == exchangeData.curAbbr) {
@@ -105,9 +124,11 @@ function NBRBExchangeToBYN(abbr, value) {
 }
 
 /**
+ * Конвертирует в иностранную валюту по аббревиатуре валюты
  * 
- * @param {*} abbr 
- * @param {*} value 
+ * @param {*} abbr аббревиатура валюты
+ * @param {*} value значение в белорусских рублях
+ * @returns значение в валюте
  */
 function NBRBExchangeFromBYN(abbr, value) {
 	if (abbr == exchangeData.curAbbr) {
@@ -123,15 +144,23 @@ function NBRBExchangeFromBYN(abbr, value) {
 }
 
 /**
+ * Конвертирует из валюты в валюту по аббревиатурам
  * 
- * @param {*} toId 
- * @param {*} fromId 
- * @param {*} value 
+ * @param {*} toId аббревиатура валюты, в которую будет конверсия
+ * @param {*} fromId аббревиатура валюты из которой будет конверсия
+ * @param {*} value значение валюты fromId
+ * @returns значение валюты toId
  */
 function NBRBExchange(toId, fromId, value) {
 	return NBRBExchangeFromBYN(toId, NBRBExchangeToBYN(fromId, value));
 }
 
+/**
+ * Получить полное название валюты по аббривеатуре
+ * 
+ * @param {*} abbr аббревиатура валюты
+ * @returns название валюты
+ */
 function NBRBGetCurNameByAbbreviation(abbr) {
 	for (var i = 0; i < exchangeData.curTab.length; i++) {
 		if (exchangeData.curTab[i].Cur_Abbreviation == abbr.toUpperCase()) {
